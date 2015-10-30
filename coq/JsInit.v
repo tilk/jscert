16fa01684a3db1a85d1bf70104035b9956427bb6
Marek Materzok <tilk@tilk.eu>
2015-10-30 14:12:57 +0100

    Fixes for initial env.

--- coq/JsInit.v
+++ coq/JsInit.v
@@ -377,7 +377,7 @@ Definition array_proto_push_function_object :=
 Definition object_prealloc_string :=
   let P := Heap.empty in
   let P := write_constant P "prototype" prealloc_string_proto in
-  object_create_prealloc_constructor prealloc_function 1 P.
+  object_create_prealloc_constructor prealloc_string 1 P.
 (* LATER *)
 
 (**************************************************************)
@@ -487,7 +487,7 @@ Definition error_proto_to_string_function_object :=
 Definition object_prealloc_native_error ne :=
   let P := Heap.empty in
   let P := write_constant P "prototype" (prealloc_native_error_proto ne) in (* Daniele: replaced 'native' with 'constant' *)
-  object_create_prealloc_constructor (prealloc_native_error_proto ne) 1 P.
+  object_create_prealloc_constructor (prealloc_native_error ne) 1 P.
 
 
 (**************************************************************)
